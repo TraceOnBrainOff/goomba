928a7c15af5c0f81442458dfde250a6848624d15
TraceOnBrainOff <sev.webmaster.k@gmail.com>
2022-02-09 16:30:28 +0100

    Installing dependencies and modules using ansible

---
+++ main.py
@@ -0,0 +1,16 @@
+import discord
+import json
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+
+token_file = open('token.txt')
+token = token_file.read()
+token_file.close()
+client.run(token)

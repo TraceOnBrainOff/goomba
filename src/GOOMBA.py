fd2bc8027d240d40e4801bcc4ae5448179bb4515
TraceOnBrainOff <sev.webmaster.k@gmail.com>
2022-02-10 01:01:24 +0100

    uh oh

--- src/GOOMBA.py
+++ src/GOOMBA.py
@@ -6,5 +6,6 @@ class GOOMBA_AutoShardedClient(discord.AutoShardedClient):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
 
+    @bot.event
     async def on_message(self, message):
         pass

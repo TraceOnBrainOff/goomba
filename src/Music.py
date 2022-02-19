d0ed5eab9fec162115ed12662f8435d312b91a41
Pi Me <sev.webmaster.k@gmail.com>
2022-02-19 15:06:38 +0100

    Music largely integrated, Queues need fixing

--- src/Music.py
+++ src/Music.py
@@ -6,22 +6,19 @@ import typing
 
 class VoiceState:
     def __init__(self, bot):
-        self.current = None
         self.voice: wavelink.Player = None
         self.bot = bot
-        self.play_next_song = asyncio.Event()
         self.songs = wavelink.Queue()
-        self.audio_player = self.bot.loop.create_task(self.audio_player_task())
+        self.original_channel: discord.TextChannel = None
 
     def is_playing(self):
-        if self.voice is None or self.current is None:
+        if self.voice is None:
             return False
-        player = self.current.player
-        return not player.is_done()
+        return self.voice.is_playing()
 
     @property
     def player(self):
-        return self.current.player
+        return self.voice
 
     def skip(self):
         if self.is_playing():
@@ -29,14 +26,13 @@ class VoiceState:
 
     def toggle_next(self):
         self.bot.loop.call_soon_threadsafe(self.play_next_song.set)
-
-    async def audio_player_task(self):
-        while True:
-            self.play_next_song.clear()
-            self.current = await self.songs.get()
-            await self.bot.send_message(self.current.channel, 'Now playing ' + str(self.current))
-            self.current.player.start()
-            await self.play_next_song.wait()
+    
+    @commands.Cog.listener()
+    async def on_wavelink_track_end(player: wavelink.Player, track: wavelink.Track, reason):
+        """Event fired when a song has ended."""
+        if not player.state.songs.is_empty(): #if there's something in the queue
+            new_track = await player.play(player.state.songs.pop())
+            await player.state.original_channel.send(f'**Now playing:** `{new_track.title}` Requested by: `{new_track.requested_by}`')
 
 class WaveMusic(commands.Cog):
     """Music cog to hold Wavelink related commands and listeners."""
@@ -57,14 +53,11 @@ class WaveMusic(commands.Cog):
         )
 
     def get_voice_state(self, server):
-        state = self.voice_states.get(server.id)
+        state = self.voice_states.get(str(server.id))
         if state is None:
             state = VoiceState(self.bot)
-            self.voice_states[server.id] = state
+            self.voice_states[str(server.id)] = state
         return state
-
-    def is_playing(self):
-        pass
 
     @commands.Cog.listener()
     async def on_wavelink_node_ready(self, node: wavelink.Node):
@@ -72,101 +65,111 @@ class WaveMusic(commands.Cog):
         print(f'Node: <{node.identifier}> is ready!')
 
     @commands.command(pass_context=True, no_pm=True)
-    async def join(self, ctx):
+    async def join(self, ctx: commands.Context):
         #check if the author is in vc
         #check if it's already connected to the channel
         #if its in the wrong channel, move it
         """Summons the bot to join your voice channel."""
         if ctx.author.voice is None:
-            await self.bot.say('You are not in a voice channel.')
+            await ctx.send('You are not in a voice channel.')
             return False
         summoned_channel = ctx.author.voice.channel
-        state = self.get_voice_state(ctx.message.server)
+        state = self.get_voice_state(ctx.message.guild)
         if state.voice is None:
             state.voice = await summoned_channel.connect(cls=wavelink.Player)
+            setattr(state.voice, "state", state) #very fucking weird reference fuckery necessary for later listener manipulation
         else:
             await state.voice.move_to(summoned_channel)
+        state.original_channel = ctx.message.channel
         return True
     
     @commands.command(pass_context=True, no_pm=True)
     async def skip(self, ctx):
-        state = self.get_voice_state(ctx.message.server)
+        state = self.get_voice_state(ctx.message.guild)
         if not state.is_playing():
-            await self.bot.say('Not playing any music right now...')
+            await ctx.send('Not playing any music right now...')
             return
-        state.skip()
-        await self.bot.say('Skipping...')
+        state.player.stop()
+        await ctx.send('Skipping...')
 
     @commands.command()
-    async def play(self, ctx: commands.Context, *, search: typing.Union[wavelink.SoundCloudTrack, wavelink.YouTubeTrack]):
+    async def play(self, ctx: commands.Context, *, track: typing.Union[wavelink.YouTubeTrack, wavelink.SoundCloudTrack]):
         """Play a song with the given search query.
 
         If not connected, connect to our voice channel.
         """
-        state = self.get_voice_state(ctx.message.server)
+        state = self.get_voice_state(ctx.message.guild)
+        #if not connected, join
         if state.voice is None:
             success = await ctx.invoke(self.join)
             if not success:
                 return
-        #if not connected, join
-        #check if it's in the same channel as the author, if not, move
+        #try:
+        #    track = await wavelink.SearchableTrack.convert(ctx, search)
+        #except:
+        #    await ctx.send(f"No results found.")
+        #    return
+        if not track:
+            await ctx.send(f"No results found.")
+            return
+        setattr(track, "requested_by", ctx.author)
         #check if already playing, if yes, add to queue, if not, play
-        if not ctx.voice_client:
-            vc: wavelink.Player = await ctx.author.voice.channel.connect()
+        if not state.is_playing():
+            await state.player.play(track)
         else:
-            vc: wavelink.Player = ctx.voice_client
-
-        await vc.play(search)
+            state.songs.put(track)
     
     @commands.command(pass_context=True, no_pm=True)
     async def playing(self, ctx):
-        state = self.get_voice_state(ctx.message.server)
-        if state.current is None:
-            await self.bot.say('Not playing anything.')
+        state = self.get_voice_state(ctx.message.guild)
+        if not state.is_playing():
+            await ctx.send('Not playing anything.')
         else:
-            await self.bot.say('Now playing {}'.format(state.current))
+            await ctx.send('Now playing {}'.format(state.player.track))
 
     
     @commands.command(pass_context=True, no_pm=True)
-    async def volume(self, ctx, value : int):
+    async def volume(self, ctx, value : int = 100):
         """Sets the volume of the currently playing song."""
-        state = self.get_voice_state(ctx.message.server)
+        state = self.get_voice_state(ctx.message.guild)
         if state.is_playing():
-            player = state.voice
-            player.volume = value / 100
-            await self.bot.say('Set the volume to {:.0%}'.format(player.volume))
+            await state.player.set_volume(value)
 
     @commands.command(pass_context=True, no_pm=True)
     async def pause(self, ctx):
         """Pauses the currently played song."""
-        state = self.get_voice_state(ctx.message.server)
+        state = self.get_voice_state(ctx.message.guild)
         if state.is_playing():
-            player = state.voice
-            player.pause()
+            await state.player.pause(True)
 
     @commands.command(pass_context=True, no_pm=True)
     async def resume(self, ctx):
         """Resumes the currently played song."""
-        state = self.get_voice_state(ctx.message.server)
-        if state.is_playing():
-            player = state.voice
-            player.resume()
+        state = self.get_voice_state(ctx.message.guild)
+        if not state.is_playing():
+            await state.player.resume()
 
     @commands.command(pass_context=True, no_pm=True)
     async def stop(self, ctx):
         """Stops playing audio and leaves the voice channel.
         This also clears the queue.
         """
-        server = ctx.message.server
-        state = self.get_voice_state(server)
+        guild = ctx.message.guild
+        state = self.get_voice_state(guild)
 
         if state.is_playing():
-            player = state.voice
-            player.stop()
+            await state.player.stop()
 
         try:
-            state.audio_player.cancel()
-            del self.voice_states[server.id]
-            await state.voice.disconnect()
+            del self.voice_states[str(guild.id)]
+            await state.player.disconnect()
         except:
             pass
+
+    def __unload(self):
+        for state in self.voice_states.values():
+            try:
+                if state.voice:
+                    self.bot.loop.create_task(state.voice.disconnect())
+            except:
+                pass
